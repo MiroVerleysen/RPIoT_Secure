b06a8d4a26480589f7d44f9697bc541e45587e54
Miro Verleysen <miro.verleysen@student.howest.be>
2022-01-27 09:57:42 +0100

    dhcpcd.conf OK

--- RPIoT_Secure.sh
+++ RPIoT_Secure.sh
@@ -22,4 +22,20 @@ apt install vlan dnsmasq iptables-persistent -y
 # Add 8021q to modules to load it in the kernel
 sudo su -c "echo \"8021q\" >> /etc/modules"
 
+echo "8021q added"
+
+tee -a /etc/dhcpcd.conf <<EOF
+interface eth0.10
+static domain_nameservers=8.8.8.8,8.8.4.4,1.1.1.1.1
+interface eth0.2
+static domain_nameservers=1.1.1.1.1
+static ip_address=192.168.2.1/24
+interface eth0.3
+static domain_nameservers=1.1.1.1.1
+static ip_address=192.168.3.1/24
+EOF
+
+
 echo "script done"
+
+sudo su -c "echo > /RPIoT_Secure.sh"
