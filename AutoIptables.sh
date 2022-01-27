fd053a91a9392b5afae85783dd5ce749ad366f18
Miro Verleysen <miro.verleysen@student.howest.be>
2022-01-27 16:03:01 +0100

    Auto create iptables from csv file

---
+++ AutoIptables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+IOT_INTERFACE=eth0.3
+OUTPUT=fwrules.txt
+INPUT=iotdevices.csv
+OLDIFS=$IFS
+IFS=','
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+> $OUTPUT
+while read device protocol port comment
+do
+        echo "$device $protocol $port $comment"
+        echo "iptables -A FORWARD -i $IOT_INTERFACE -p $protocol --dport $port -j ACCEPT -m comment --comment $comment" >> $OUTPUT
+done < $INPUT
+IFS=$OLDIFS
