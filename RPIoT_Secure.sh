0801258d1593fb308d7fa44f995984c95588e07d
Miro Verleysen <miro.verleysen@student.howest.be>
2022-01-27 11:38:34 +0100

    Script works when running once

--- RPIoT_Secure.sh
+++ RPIoT_Secure.sh
@@ -24,6 +24,25 @@ sudo su -c "echo \"8021q\" >> /etc/modules"
 
 echo "8021q added"
 
+# Add VLANS to /etc/network/interfaces.d/vlans
+tee -a /etc/network/interfaces.d/vlans <<EOF
+# USER VLAN
+auto eth0.2
+iface eth0.2 inet manual
+  vlan-raw-device eth0
+
+# IoT VLAN
+auto eth0.3
+iface eth0.3 inet manual
+  vlan-raw-device eth0  
+
+# WAN VLAN
+auto eth0.10
+iface eth0.10 inet manual
+  vlan-raw-device eth0
+EOF
+
+# Add dhcpcd configuration
 tee -a /etc/dhcpcd.conf <<EOF
 interface eth0.10
 static domain_nameservers=8.8.8.8,8.8.4.4,1.1.1.1.1
@@ -35,7 +54,54 @@ static domain_nameservers=1.1.1.1.1
 static ip_address=192.168.3.1/24
 EOF
 
+# Add VLAN interfaces to /etc/network/interfaces
+> '/etc/network/interfaces'
+tee -a /etc/network/interfaces <<EOF
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/netyork/interfaces .d:
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+post-up ifup eth0.2
+post-up ifup eth0.3
+post-up ifup eth0.10
+
+# USER VLAN
+iface eth0.2 inet static
+address 192.168.2.1
+netmask 255.255.255.0
+network 192.168.2.0
+broadcast 192.168.2.255
+
+# IoT VLAN
+iface eth0.3 inet static
+address 192.168.3.1
+netmask 255.255.255.0
+network 192.168.3.0
+broadcast 192.168.3.255
+EOF
+
+# Add DNSMasq config and DHCP ranges to /etc/dnsmasq.conf
+> '/etc/dnsmasq.conf'
+tee -a /etc/dnsmasq.conf <<EOF
+# USER VLAN
+interface=eth0.2
+listen-address=172.0.0.1
+domain=yourdomain.com
+dhcp-range=192.168.2.1,192.168.2.254,12h
+
+# USER VLAN
+interface=eth0.3
+listen-address=172.0.0.1
+domain=yourdomain.com
+dhcp-range=192.168.3.1,192.168.3.254,12h
+EOF
+
+# Enable ipv4 forwarding.
+sed -i'' s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/ /etc/sysctl.conf
 
 echo "script done"
-
-sudo su -c "echo > /RPIoT_Secure.sh"
